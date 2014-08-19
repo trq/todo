1d4c209d6a17fefab9e6e223eca2a1c28a813e12
Tony R Quilkey <trq@thorpesystems.com>
2014-08-19 23:16:12 +1000

    Initial work pulling data from the api.

--- web/js/app.js
+++ web/js/app.js
@@ -1,1 +1,11 @@
-$(document).foundation();
+(function() {
+    var app = angular.module('todo', []);
+
+    app.controller('TodoController', ['$http', function($http) {
+        var self = this;
+
+        $http.get('/api/todos', {headers: {Accept: 'application/json'}}).success(function(data) {
+            self.items = data.todos;
+        });
+    }]);
+})();
